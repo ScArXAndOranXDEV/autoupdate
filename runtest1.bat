60021dffd6a7eb090799ed8409ba76b7e8465a84
ScArXAndOranXDEV <121250463+ScArXAndOranXDEV@users.noreply.github.com>
2022-12-23 17:38:41 +0200

    Add files via upload

---
+++ runtest1.bat
@@ -0,0 +1,20 @@
+@echo off
+:check
+curl -L -o autoupdate.txt https://raw.githubusercontent.com/ScArXAndOranXDEV/autoupdate/main/autoupdate.txt
+for /f "Delims=" %%a in (autoupdate.txt) do (
+
+set TEXT=%%a
+
+)
+
+if 1.0==%TEXT% goto no
+echo update found
+pause
+goto start
+
+
+
+:no
+echo update not found
+pause
+goto start
